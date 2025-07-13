d58725f8e1cb0de6fb6a773fd5dfe030e8da6784
Ashutosh Gupta <154120097+ashutoshSieve@users.noreply.github.com>
2025-07-13 10:59:50 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://roommatefinder3.netlify.app"],
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
