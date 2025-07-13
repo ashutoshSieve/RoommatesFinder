7345f3e6d78cd7eaa799c6bfd6d2ecd5c011a85b
Ashutosh Gupta <154120097+ashutoshSieve@users.noreply.github.com>
2025-07-13 12:13:54 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -306,14 +306,14 @@ app.post("/api/signup", async(req, res) =>{
 
         res.cookie("jwt", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production" ? true : false,
+            secure: true,
             sameSite: "None"
         });
 
         res.status(201).json({ message: "User created successfully!", token });
 
     } catch (error) {
-        res.status(500).json({ message: "Error creating user", error });
+        res.status(500).json({ message: "Error creating user", error.message });
     }
 });
 
@@ -329,10 +329,10 @@ app.post("/api/login", async(req, res) =>{
         const token = generateJWT({ id: user._id, name: user.name, email: user.email });
         res.cookie('jwt', token, { httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "None" });
 
-        res.status(200).json({ message: "User logged in successfully!" });
+        res.status(200).json({ message: "User logged in successfully!", token });
 
     } catch (error) {
-        res.status(500).json({ message: "Error logging in", error });
+        res.status(500).json({ message: "Error logging in", error.message });
     }
 });
 
